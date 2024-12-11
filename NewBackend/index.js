4bf43c5deb2e3a3ce82d9a85f5f0689375a19d4a
Hammad-12345 <hafizmuhammadhammad503@gmail.com>
2024-12-11 13:19:44 +0500

    code push you

--- NewBackend/index.js
+++ NewBackend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 3000;
-
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
