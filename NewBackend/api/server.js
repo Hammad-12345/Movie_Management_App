a361394fa683c2c1bbed1ab9202f9bff9a5287d6
Hammad-12345 <hafizmuhammadhammad503@gmail.com>
2024-12-14 15:31:56 +0500

    push deployment

--- NewBackend/api/server.js
+++ NewBackend/api/server.js
@@ -1,38 +1,46 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const dotenv = require("dotenv");
-const { connectiondb } = require("./DB/Connect");
+const { connectiondb } = require("../DB/Connect");
 const axios = require("axios");
-const { userrouter } = require('./mvc/route/AuthRoutes/UserAuthRoutes')
-const { FavouriteMovierouter } = require('./mvc/route/FavouriteMovieRoutes/FavouriteMovieRoute')
+const { userrouter } = require("../mvc/route/AuthRoutes/UserAuthRoutes");
+const { FavouriteMovierouter } = require("../mvc/route/FavouriteMovieRoutes/FavouriteMovieRoute");
+
 // Load environment variables from .env file
 dotenv.config();
 
-// connect with database
+// Create the Express app
+const app = express();
+
+// Connect with the database
 connectiondb();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Root route
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+// Movies list route
 app.get("/movieslist", async (req, res) => {
   try {
     const response = await axios.get(
       "https://itunes.apple.com/search?term=star&country=au&media=movie&all"
     );
-    res.send(response.data); // Return the response to the frontend
+    res.send(response.data);
   } catch (error) {
     res.status(500).json({ error: "Error fetching data from iTunes API" });
   }
 });
-app.use("/user", userrouter);
-app.use("/Favourite",FavouriteMovierouter)
 
-// listen server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// User routes
+app.use("/user", userrouter);
+
+// Favourite movie routes
+app.use("/favourite", FavouriteMovierouter);
+
+// Export the app as a Vercel serverless function
+module.exports = app;
